fb97cda794ffbfdec3c71b451785daf5dae500d0
Yukai Wang <51202049+wykswr@users.noreply.github.com>
2023-06-04 21:37:19 -0700

    Yukai (#9)
    * progress report1
    
    * Review (#6)
    
    * business logic design
    
    * progress report1 (#5)
    
    * boilerplate

---
+++ front-end/src/pages/Login.js
@@ -0,0 +1,9 @@
+function Login() {
+    return (
+        <div className={"container mx-auto"}>
+            <h1 className={"text-green-500"}>Login</h1>
+        </div>
+    );
+}
+
+export default Login;
