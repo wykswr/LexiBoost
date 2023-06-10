09b4ec8bd17360f2cbd5f34652797c3781be8eaa
alkhatibmahmoud <59523244+alkhatibmahmoud@users.noreply.github.com>
2023-06-10 09:39:10 -0700

    Nav bar (#12)
    * business logic design
    
    * progress report1 (#5)
    
    * Added drafts for some pages we may implement. They might include some sections related to stretch goal. (#7)
    
    * Yukai (#9)
    
    * progress report1
    
    * Review (#6)
    
    * business logic design
    
    * progress report1 (#5)
    
    * boilerplate
    
    * Add sign in and sign up components (#10)
    
    * Add Nav Bar
    
    ---------
    
    Co-authored-by: wangyk <wangyukai159@outlook.com>
    Co-authored-by: Yukai Wang <51202049+wykswr@users.noreply.github.com>
    Co-authored-by: Samuel Yang <70651651+inside1D@users.noreply.github.com>
    Co-authored-by: serajsas <64566514+serajsas@users.noreply.github.com>

---
+++ front-end/src/components/NavBar.js
@@ -0,0 +1,20 @@
+import {Link} from "react-router-dom";
+
+
+function NavBar() {
+    return (
+        <nav className={"flex flex-row justify-around"}>
+            <div className={"bg-blue-400 rounded"}>
+                <Link to="/">Home</Link>
+            </div>
+            <div className={"bg-pink-500 rounded"}>
+                <Link to="/login">Login</Link>
+            </div>
+            <div className={"bg-pink-500 rounded"}>
+                <Link to="/signup">SignUp</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
