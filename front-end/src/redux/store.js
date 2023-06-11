9f5e97b8b3d7f321600d6bfdcece6ea39ff6b040
Seraj Abo Sabbah <serajaldinabosabbah@gmail.com>
2023-06-11 11:07:37 -0700

    Revert "Add code for login and sign up properly styled"
    This reverts commit 6d91145ea9e1e78307d451856a51b994837a57af.

--- front-end/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './auth/reducer';
-
-export const store = configureStore({
-    reducer: {
-        auth: authReducer
-    },
-    devTools: true
-});
