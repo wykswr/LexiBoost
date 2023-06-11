12ddfe53eb88bb01acbe9e213b9df5b8e8857df8
mahmoudalkhatib <mahmoudalkhatib.ubc@gmail.com>
2023-06-10 21:02:01 -0700

    Create Backend Boilerplate

---
+++ backend/controllers/userController.js
@@ -0,0 +1,14 @@
+// userController.js
+const UserModel = require('../models/userModel');
+
+// Example controller function
+const getUsers = async (req, res) => {
+    try {
+        const users = await UserModel.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+module.exports = { getUsers };
