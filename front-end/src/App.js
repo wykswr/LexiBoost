10a6bab5323dafea8945d3c12b9d5b9678c17be2
UBC Student <joycy@students.cs.ubc.ca>
2023-06-21 22:18:40 -0700

    add card learning page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 import Demo from "./pages/Demo";
 import BookshelfPage from "./pages/BookshelfPage";
+import CardLearning from "./pages/CardLearning";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/" element={<AuthPage />}/>
                 <Route path="/demo" element={<Demo />}/>
                 <Route path="/bookshelf" element={<BookshelfPage />}/>
+                <Route path="/cardlearning" element={<CardLearning/>}/>
             </Routes>
         </div>
     );
