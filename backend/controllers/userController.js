e02d205f4e60a907b89decf5a959c4e32181fa75
mahmoudalkhatib <mahmoudalkhatib.ubc@gmail.com>
2023-06-22 18:57:09 -0700

    User CRUD Operations

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,90 @@
-// userController.js
-const UserModel = require('../models/userModel');
+const User = require('../models/user');
 
-// Example controller function
-const getUsers = async (req, res) => {
-  try {
-    const users = await UserModel.find();
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({error: err.message});
-  }
+const createUser = async (req, res) => {
+    try {
+        const { avatar, name, email_address} = req.body;
+
+        const newUser = new User({
+            avatar,
+            name,
+            email_address,
+        });
+
+        const createdUser = await newUser.save();
+
+        res.status(201).json(createdUser);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 
-module.exports = {getUsers};
+const getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const getUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const user = await User.findById(id);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { avatar, name, email_address} = req.body;
+
+        const updatedUser = await User.findByIdAndUpdate(
+            id,
+            { avatar, name, email_address},
+            { new: true }
+        );
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const deletedUser = await User.findByIdAndRemove(id);
+
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json({ message: 'User deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = {
+    createUser,
+    getUsers,
+    getUserById,
+    updateUser,
+    deleteUser
+};
