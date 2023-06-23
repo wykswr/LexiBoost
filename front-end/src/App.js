4459aac828f3b77158a45b59d0847329cbe28660
inside1D <samuelxingbo@gmail.com>
2023-06-22 17:20:34 -0700

    Implemented the DeckDetail page for the market

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,8 @@
 import {Route, Routes} from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
-import HomePage from "./pages/HomePage";
 import Demo from "./pages/Demo";
 import BookshelfPage from "./pages/BookshelfPage";
-
+import DeckDetail from "./components/DeckDetail";
 function App() {
     return (
         <div className="App">
@@ -11,6 +10,8 @@ function App() {
                 <Route path="/" element={<AuthPage />}/>
                 <Route path="/demo" element={<Demo />}/>
                 <Route path="/bookshelf" element={<BookshelfPage />}/>
+                <Route path="/test_detail" element={<DeckDetail />}/>
+
             </Routes>
         </div>
     );
--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/reducer';
-
+import deckDetailReducer from './deckDetail/reducer'
 export const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        deckDetail: deckDetailReducer
     },
     devTools: true
 });
