8d22074cf22a9b5a0d03ff12264b608c864b862d
mahmoudalkhatib <mahmoudalkhatib.ubc@gmail.com>
2023-07-03 19:15:26 -0700

    User log in function in model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,9 +1,10 @@
 // connect to mongo
 const mongoose = require('mongoose');
-
+const { v4: uuid } = require('uuid');
+const bcrypt = require('bcrypt')
 // Define the user schema
 const userSchema = new mongoose.Schema({
-    id: { type: Number, required: true, unique: true },
+    id: { type: String, required: true, unique: true },
     creation_date: { type: Date, required: true },
     auth_token: { type: String, required: true },
     email: { type: String },
@@ -14,6 +15,32 @@ const userSchema = new mongoose.Schema({
     avatar: { type: String }, // Assuming the avatar is stored as a file path or URL
     decks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Deck' }]
 });
+
+userSchema.statics.createUser = async function (first_name, last_name, email, password) {
+    try {
+        const newUser = new this({
+            id: uuid(),
+            creation_date: new Date(),
+            auth_token: generateAuthToken(),
+            email,
+            first_name,
+            last_name,
+            password: await bcrypt.hash(password, 10),
+            decks: []
+        });
+
+        const user = await newUser.save();
+
+        const token = generateAuthToken();
+        user.auth_token = token
+        await user.save()
+
+        return user;
+    } catch (error) {
+        console.error('Error creating user:', error);
+        throw new Error('Failed to create user');
+    }
+};
 
 // Create the User model
 const User = mongoose.model('User', userSchema);
