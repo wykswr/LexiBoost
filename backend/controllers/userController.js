2070f6ab2bbcf8ed81e30ff612a5206a44cb1877
serajsas <64566514+serajsas@users.noreply.github.com>
2023-07-04 15:38:57 -0700

    Backend decks (#37)
    * Add decks endpoints
    
    * Fix append flashcard endpoint
    
    * Edit typo
    
    * Edit schemas
    
    * Add document validations
    
    * Cleanup
    
    * Improvements
    
    * Add more deck routes
    
    * Fix get user decks endpoint
    
    * Add flashcard crud
    
    * Edit update to only override provided fields
    
    * Add burnt and sorting to flashcards
    
    * Fix based on feedback

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,12 +3,12 @@ const UserModel = require('../models/userModel');
 
 // Example controller function
 const getUsers = async (req, res) => {
-    try {
-        const users = await UserModel.find();
-        res.json(users);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  try {
+    const users = await UserModel.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({error: err.message});
+  }
 };
 
-module.exports = { getUsers };
+module.exports = {getUsers};
