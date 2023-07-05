4016519b53476e851a2fc4ed37d3a5b3d2c8e249
inside1D <samuelxingbo@gmail.com>
2023-07-05 11:06:30 -0700

    Modified Styling of profile page

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/reducer';
 import cardCreationReducer from './card_creation/reducer';
 import marketPlaceReducer from './marketPlace/reducer';
-import deckDetailReducer from './deckDetail/reducer'
+import deckDetailReducer from './deckDetail/reducer';
+import profileEditingReducer from './userProfile/reducer';
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         deckDetail: deckDetailReducer,
         creationForm: cardCreationReducer,
+        profileEditingForm: profileEditingReducer,
         marketPlace: marketPlaceReducer,
     },
     devTools: true
