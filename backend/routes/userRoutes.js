6a74be772c94ef7f1f5c05d893945a7c63c644ff
Yukai Wang <51202049+wykswr@users.noreply.github.com>
2023-07-26 21:38:01 -0700

    Visibility (#57)
    * visibility & CD
    
    * visibility & CD
    
    * CD & user endpoint
    
    * CD & user endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,14 @@
 // userRoutes.js
 const express = require('express');
 const userController = require('../controllers/userController');
+const verifyToken = require('../middleware/auth');
 
 const router = express.Router();
 
 // Example route
 router.post('/login', userController.loginUser);
 router.post('/signup', userController.createUser);
-router.get('/:userId', userController.getUser);
-router.put('/:userId', userController.editUser);
+router.get('/', verifyToken, userController.getUser);
+router.put('/', verifyToken, userController.editUser);
 
 module.exports = router;
