95c65e670f8d6890c8eef09ee94529cca0698687
inside1D <samuelxingbo@gmail.com>
2023-08-02 19:51:10 -0700

    Changes: 1.Add detail button for marketplace 2.Using Seraj's changes, fixed the pagination of marketplace

--- client/src/pages/UserProfileUpdated.jsx
+++ client/src/pages/UserProfileUpdated.jsx
@@ -1,25 +1,33 @@
+import {useDispatch, useSelector} from "react-redux";
 import { Tab } from '@headlessui/react'
+import {FireIcon, RectangleStackIcon, Square3Stack3DIcon, UserCircleIcon} from "@heroicons/react/20/solid";
+import {flipVisibility} from "../redux/userProfile/reducer";
 import {Cog8ToothIcon} from "@heroicons/react/24/solid";
 import ProfileEditingForm from "../components/ProfileEditingForm";
 import {
     useGetUserProfileQuery,
+    usePublishDeckMutation,
+    useRetractDeckMutation
 } from "../redux/api/apiSlice.js";
 
 import DeckPreviewList from "../components/DeckPreviewList.jsx";
-import ToLogin from "../components/ToLogin.jsx";
-import NavBar from "../components/shared/NavBar.jsx";
-import {Base64, decode} from 'js-base64';
-
 const UserProfileUpdated = ({id}) => {
     const {data, isLoading, isError} = useGetUserProfileQuery();
+    const [publishDeck] = usePublishDeckMutation();
+    const [retractDeck] = useRetractDeckMutation();
 
     if (isLoading) return <div>Loading...</div>
-    if (isError) return <ToLogin/>;
+    if (isError) return <div>Error</div>
 
+    const setEnabled = () => {
+        if (data.deck.isPublic) {
+            retractDeck(id);
+        } else {
+            publishDeck(id);
+        }
+    }
     return (
-        <>
-            <NavBar/>
-            <div className={"h-screen container pt-16 mx-auto flex flex-col mx"}>
+        <div className={"h-screen container pt-16 mx-auto flex flex-col mx"}>
             <Tab.Group vertical defaultIndex={1}>
                 <Tab.List className="flex md:flex-row justify-center" >
                     <Tab className="bg-gray-500 hover:bg-gray-600 py-2 px-4 rounded-t-lg"> <Cog8ToothIcon className="h-5 w-5 text-white"/> </Tab>
@@ -37,7 +45,7 @@ const UserProfileUpdated = ({id}) => {
                             <div className="bg-white shadow-lg rounded-lg p-4 md:p-8 md:col-span-4">
                                 <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 items-center">
                                     <div className="w-full md:w-1/3 flex items-center justify-center">
-                                        <img className="h-40 w-40 bg-gray-300 rounded-full" src={decode(data.avatar)} alt="User Avatar" />
+                                        <img className="h-40 w-40 bg-gray-300 rounded-full" src={data.avatar} alt="User Avatar" />
                                     </div>
                                     <div className="flex flex-col justify-center">
                                         <div className="text-lg font-medium text-gray-800">User: {data.firstName}</div>
@@ -54,7 +62,16 @@ const UserProfileUpdated = ({id}) => {
                                         </div>
                                     </div>
                                 </div>
-
+                                <div className="flex flex-col md:flex-row justify-between mt-8">
+                                    <div className="flex items-center space-x-2 mb-4 md:mb-0">
+                                        <FireIcon className="h-5 w-5 text-red-500" />
+                                        <span className="text-gray-800">You have studied 114,514 cards</span>
+                                    </div>
+                                    <div className="flex items-center space-x-2">
+                                        <RectangleStackIcon className="h-5 w-5 text-blue-500" />
+                                        <span className="text-gray-800">You have studied 1,919,810 decks</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </Tab.Panel>
@@ -64,7 +81,6 @@ const UserProfileUpdated = ({id}) => {
                 </Tab.Panels>
             </Tab.Group>
         </div>
-        </>
     )
 
 }
