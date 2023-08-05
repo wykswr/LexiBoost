d489666628670bf4294b7c8940490d3151ddcac0
inside1D <samuelxingbo@gmail.com>
2023-08-05 00:13:57 -0700

    added missing files

--- client/src/redux/api/apiSlice.js
+++ client/src/redux/api/apiSlice.js
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({baseUrl: import.meta.env.VITE_API_URL, credentials: 'include'}),
-    tagTypes: ['bookshelf', 'singleDeck', 'UserProfile', 'singleCard'],
+    tagTypes: ['bookshelf', 'singleDeck', 'UserProfile', 'singleCard', 'allCards'],
     endpoints: builder => ({
         getDecks: builder.query({
             query: () => '/decks',
@@ -67,6 +67,7 @@ export const apiSlice = createApi({
         }),
         getFlashCards: builder.query({
            query: id => `/decks/${id}/flashcards`,
+            providesTags: ['allCards']
         }),
         getSingleFlashCard: builder.query({
             query: ({deckId, cardId}) => `/decks/${deckId}/flashcards/${cardId}`,
@@ -126,7 +127,7 @@ export const apiSlice = createApi({
                 url: `/decks/${deckId}/flashcards/${cardId}`,
                 method: 'DELETE'
             }),
-            invalidatesTags: ['singleCard']
+            invalidatesTags: ['allCards']
         }),
         updateFlashCard: builder.mutation({
             query: ({deckId, cardId, content}) => ({
@@ -141,7 +142,8 @@ export const apiSlice = createApi({
                 url: `/decks/${deckId}/flashcards`,
                 method: 'POST',
                 body: content
-            })
+            }),
+            invalidatesTags: ['allCards']
         })
 
     }),
@@ -169,5 +171,5 @@ export const {
     useLoginMutation,
     useDeleteFlashCardMutation,
     useUpdateFlashCardMutation,
-    useAddFlashCardMutation
+    useAddFlashCardMutation,
 } = apiSlice;
