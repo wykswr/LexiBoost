5d438983d22d0fe37d526c87e38462ff600b5ce1
inside1D <samuelxingbo@gmail.com>
2023-08-06 17:22:32 -0700

    Removed Unnecessary profile component

--- client/src/pages/UserProfileUpdated.jsx
+++ client/src/pages/UserProfileUpdated.jsx
@@ -1,31 +1,17 @@
-import {useDispatch, useSelector} from "react-redux";
 import { Tab } from '@headlessui/react'
-import {FireIcon, RectangleStackIcon, Square3Stack3DIcon, UserCircleIcon} from "@heroicons/react/20/solid";
-import {flipVisibility} from "../redux/userProfile/reducer";
 import {Cog8ToothIcon} from "@heroicons/react/24/solid";
 import ProfileEditingForm from "../components/ProfileEditingForm";
 import {
     useGetUserProfileQuery,
-    usePublishDeckMutation,
-    useRetractDeckMutation
 } from "../redux/api/apiSlice.js";
 
 import DeckPreviewList from "../components/DeckPreviewList.jsx";
 const UserProfileUpdated = ({id}) => {
     const {data, isLoading, isError} = useGetUserProfileQuery();
-    const [publishDeck] = usePublishDeckMutation();
-    const [retractDeck] = useRetractDeckMutation();
 
     if (isLoading) return <div>Loading...</div>
     if (isError) return <div>Error</div>
 
-    const setEnabled = () => {
-        if (data.deck.isPublic) {
-            retractDeck(id);
-        } else {
-            publishDeck(id);
-        }
-    }
     return (
         <div className={"h-screen container pt-16 mx-auto flex flex-col mx"}>
             <Tab.Group vertical defaultIndex={1}>
@@ -62,16 +48,7 @@ const UserProfileUpdated = ({id}) => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="flex flex-col md:flex-row justify-between mt-8">
-                                    <div className="flex items-center space-x-2 mb-4 md:mb-0">
-                                        <FireIcon className="h-5 w-5 text-red-500" />
-                                        <span className="text-gray-800">You have studied 114,514 cards</span>
-                                    </div>
-                                    <div className="flex items-center space-x-2">
-                                        <RectangleStackIcon className="h-5 w-5 text-blue-500" />
-                                        <span className="text-gray-800">You have studied 1,919,810 decks</span>
-                                    </div>
-                                </div>
+
                             </div>
                         </div>
                     </Tab.Panel>
