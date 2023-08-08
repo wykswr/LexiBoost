546ab4ed76fbad5b64af2a9361ffbc5e16b1efa1
UBC Student <joycy@students.cs.ubc.ca>
2023-08-08 00:41:33 -0700

    remove local record files

--- client/src/redux/api/apiSlice.js
+++ client/src/redux/api/apiSlice.js
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({baseUrl: import.meta.env.VITE_API_URL, credentials: 'include'}),
-    tagTypes: ['bookshelf', 'singleDeck', 'UserProfile', 'singleCard'],
+    tagTypes: ['bookshelf', 'singleDeck', 'UserProfile', 'singleCard', 'allCard'],
     endpoints: builder => ({
         getDecks: builder.query({
             query: () => '/decks',
